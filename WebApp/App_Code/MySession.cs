8eaad69e1f3e535d5bafc7b322e496ab3718840c
VMarisevs <vmarisevs@gmail.com>
2015-03-28 13:31:25 +0000

    Login working
    - Login.aspx code moved to cs
    - The landing page is ScoreBoard.aspx after users logged
    - on the page we can get the user id from MySession class

---
+++ WebApp/App_Code/MySession.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+/// <summary>
+/// Summary description for MySession
+/// </summary>
+public class MySession
+{
+    public MySession()
+    {
+        //in case we need to initialize any variable once session started
+        UserId = Membership.GetUser().ProviderUserKey.ToString();
+    }
+
+    public static MySession Current
+    {
+        get
+        {
+            MySession session = (MySession)HttpContext.Current.Session["__MySession__"];
+            if (session == null)
+            {
+                session = new MySession();
+                HttpContext.Current.Session["__MySession__"] = session;
+            }
+            return session;
+        }
+    }
+
+    public string UserId { get; set; }
+
+    /**
+     *  we can add extra parameters in here, like
+     *  
+     *  public string Property1 { get; set; }
+     *  public DateTime MyDate { get; set; }    
+     *  public int LoginId { get; set; }
+     *  
+     * 
+     * and use this class anywhere (Page_Load)
+     * 
+     * string mylogin = MySession.Current.LoginId;
+     * 
+     * string property1 = MySession.Current.Property1;
+     * MySession.Current.Property1 = newValue;
+     * 
+     * DateTime myDate = MySession.Current.MyDate;
+     * MySession.Current.MyDate = DateTime.Now;
+     */
+}
--- WebApp/Guest/Login.aspx
+++ WebApp/Guest/Login.aspx
@@ -1,14 +1,9 @@
-﻿<%@ Page Title="" Language="VB" MasterPageFile="~/MasterPage.master" AutoEventWireup="false" CodeFile="Login.aspx.vb" Inherits="Guest_Login" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Guest_Login" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
-    <p>
-        <br />
-        <asp:Login ID="Login1" runat="server">
-        </asp:Login>
-    </p>
-    <p>
-    </p>
+    <asp:Login ID="Login1" runat="server">
+    </asp:Login>
 </asp:Content>
 
